049adb7e253101e3ad0c1585014ee0a5b84c7f6d
Muntaha <muntahasayed396@gmail.com>
2025-05-31 03:28:04 +0530

    nothing much

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,7 +3,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import SearchBar from './SearchBar';
-import data from './data.json'
 import "../globals.css";
 
 const Hero = () => {
@@ -46,15 +45,7 @@ const Hero = () => {
       
       {/* Content overlay */}
       <div className="relative p-6 rounded z-10">
-             <SearchBar
-               data={Object.entries(data.chemicals).map(([key, values], idx) => ({
-                 id: idx.toString(),
-                 title: key,
-                 items: values
-               }))}
-             />
-             
-             {/* Display search results */}
+             <SearchBar></SearchBar>
         <motion.h2 
           className="text-4xl md:text-5xl font-bold mb-4"
           initial={{ opacity: 0, x: 200 }}
