812d3414d482bf79130ba60806ee6cdf75984064
Fareed95 <fareedsayed95@gmail.com>
2025-05-31 15:25:40 +0530

    sorted searchbar production level

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -29,8 +29,11 @@ const SearchBar = () => {
 
   // console.log(allChemicals)
 
-  const fuse = new Fuse(allChemicals)
-
+  const fuse = new Fuse(allChemicals, {
+    keys: ['name'], // or other keys you want
+    threshold: 0.6, // optional: controls fuzziness
+  });
+  
   function Clickevent() {
   if (Width === '40%') {
     setWidth('70%');
@@ -80,11 +83,12 @@ const SearchBar = () => {
          transition={{duration:0.5,ease:"easeInOut"}}
          > 
           <ul className="mt-2 bg-white/90 rounded shadow ">
-          {data.map((result, idx) => (
+          {data.map((result: Fuse.FuseResult<Chemical>, idx: number) => (
   <li key={idx} className="text-gray-700 py-4 px-2 hover:bg-gray-100 shadow">
     {result.item?.name || JSON.stringify(result.item)}
   </li>
 ))}
+
           </ul>
           </motion.div>
         )}
